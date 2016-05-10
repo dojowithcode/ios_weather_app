4a0466b646d80b5291d2c6e0e5c12407852c82c3
Set Kyar <setkyar@sets-macbook-pro.local>
2016-05-10 21:08:08 +0630

    Initial Commit

---
+++ Weather-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by Set Kyar on 5/10/16.
+//  Copyright © 2016 setkyarwalar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
