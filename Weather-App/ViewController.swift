0173717cc9c38ec0f923a8df2a98d5bd93a584de
Set Kyar <setkyar@Sets-MacBook-Pro.local>
2016-05-11 22:54:31 +0630

    add text field, ok action, cancel action ans basic layout

--- Weather-App/ViewController.swift
+++ Weather-App/ViewController.swift
@@ -9,7 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
+    @IBAction func setCityTapped(sender: UIButton) {
+        print("City button tapped")
+        openCityAlert()
+    }
+    
+    func openCityAlert() {
+        //Create alert Controller
+        let alert = UIAlertController(title: "City",
+            message: "Enter city name",
+            preferredStyle: UIAlertControllerStyle.Alert)
+        
+        //Create cancel action
+        let cancel = UIAlertAction(title: "Cancel",
+            style: UIAlertActionStyle.Cancel,
+            handler: nil)
+        
+        alert.addAction(cancel)
+        
+        //Create ok action
+        let ok = UIAlertAction(title: "OK",
+            style: UIAlertActionStyle.Default) { (action: UIAlertAction) -> Void in
+                print("OK")
+                let textField = alert.textFields?[0]
+                print(textField?.text)
+                self.cityLabel.text = textField?.text!
+        }
+        
+        alert.addAction(ok)
+        
+        //Add text field
+        alert.addTextFieldWithConfigurationHandler { (textField: UITextField) -> Void in
+            textField.placeholder = "City Name"
+        }
+        
+        //present alert controller
+        self.presentViewController(
+            alert,
+            animated: true,
+            completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
