0a1176d797abb646fe795d8c5034d47bb7060389
Set Kyar <setkyar@Sets-MacBook-Pro.local>
2016-05-12 22:28:07 +0630

    Add security plist && yeah I know api key is on the app, Will reset it later :D

--- Weather-App/WeatherService.swift
+++ Weather-App/WeatherService.swift
@@ -17,17 +17,28 @@ class WeatherService {
     var delegate: WeatherServiceDelegate?
     
     func getWeather(city: String) {
-        print("Weather Service city: \(city)")
+        
+        let path = "http://api.openweathermap.org/data/2.5/weather?q=Yangon&APPID=0e7b7d5c2172c35130275ea5cd8a79b0"
+        let url  = NSURL(string: path)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url!) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            print(">>>>> \(data)")
+        }
+        
+        task.resume()
+        
+        
+        //print("Weather Service city: \(city)")
         
         //make a request to get weahter data
         //wait...
         //process data
         
-        let weather = Weather(cityName: city, temp: 237.12, description: "A nice day")
-        
-        if delegate != nil {
-            delegate?.setWeather(weather)
-        }
+//        let weather = Weather(cityName: city, temp: 237.12, description: "A nice day")
+//        
+//        if delegate != nil {
+//            delegate?.setWeather(weather)
+//        }
     }
     
 }
